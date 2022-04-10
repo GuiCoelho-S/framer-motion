105bfc47bccb8bef760d1e34bf5c015fba03be8f
GuiCoelho-S <gs.coelho@outlook.com>
2022-04-10 19:05:10 -0300

    adicionando componentes visuais

---
+++ src/pages/FramerComponents/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+import * as S from './style';
+
+export const FramerComponent:FC = () => {
+
+  return (
+    <S.Container>
+      <p>Component framer motion</p>
+    </S.Container>
+  )
+}
---
+++ src/pages/FramerComponents/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+
+grid-area: main;
+margin:0 auto;
+
+width:100%;
+height:100%;
+min-height: calc(100vh - var(--footer-height));
+
+
+`
